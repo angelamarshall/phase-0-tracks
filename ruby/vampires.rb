a0058fb8788dd804c1534c0f30371de8564a283f
“angelamarshall” <hello@angelasmarshall.com>
2016-11-17 20:40:56 -0600

    added loop to repeat the survey according to number of employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,37 +5,37 @@
 #	puts vampires.rb
 #end
 
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
+
+ 	until employees<=0
+		puts "What is your name?"
+  		applicant_name = gets.chomp
+    	puts "How old are you?"
+  		age = gets.chomp.to_i
+    	puts "What year were you born?"
+  		year = gets.chomp.to_i
+    	now = Time.now.year
+    	correct_year = now - year
+    	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  		garlic = gets[0] == "y"
+    	puts "Would you like to enroll in the company’s health insurance?"
+  		health_insurance = gets[0] == "y"
+
+		if (age == correct_year) && ((garlic == true) || (health_insurance == true))
+			print "Probably not a vampire."
+		elsif (age != correct_year) && ((garlic == false) || (health_insurance == false))
+			print "Probably a vampire."
+		elsif ((age != correct_year) && (garlic == false) && (health_insurance == false))
+			print "Almost certainly a vampire."
+		elsif applicant_name == "Drake Cula" || "Tu Fang"
+			print "Definitely a vampire."
+		else
+			print "Results inconclusive."
+		end
+  	employees-=1
+  	end
 
 
-puts "What is your name?"
-applicant_name = gets.chomp
-
-puts "How old are you?"
-age = gets.chomp.to_i
-
-puts "What year were you born?"
-year = gets.chomp.to_i
-
-now = Time.now.year
-
-correct_year = now - year
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets[0] == "y"
-
-puts "Would you like to enroll in the company’s health insurance?"
-health_insurance = gets[0] == "y"
-
-if (age == correct_year) && ((garlic == true) || (health_insurance == true))
-	puts "Probably not a vampire."
-elsif (age != correct_year) && ((garlic == false) || (health_insurance == false))
-	puts "Probably a vampire."
-elsif ((age != correct_year) && (garlic == false) && (health_insurance == false))
-	puts "Almost certainly a vampire."
-elsif applicant_name == "Drake Cula" || "Tu Fang"
-	puts "Definitely a vampire."
-else
-	puts "Results inconclusive."
-end
-
+# This is not coming out with the proper results.... not sure what I am doing wrong at this point. The logic behind it seems legitimate to me. 
 
