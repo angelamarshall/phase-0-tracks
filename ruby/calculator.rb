0de6771fb8144ff0f939ffca39df1de9e334837f
“angelamarshall” <hello@angelasmarshall.com>
2016-11-30 11:35:52 -0500

    stored info in data structure

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,30 +1,39 @@
 def calculator(n, s, i)
   if s == "+"
-    puts n + i 
+    p n + i 
   elsif s == "-"
-    puts n - i 
+    p n - i 
   elsif s == "*"
-    puts n * i 
+    p n * i 
   elsif s == "/"
-    puts n / i
+    p n / i
   else
-    puts "error"
+    p "error (try putting a space between your values)"
   end
 end
 
-#calculator(4, '+', 5)
 
-#Driver Code
+#calculator(4, '+', 5)
+calc_stored = []
+
 puts "What calculation would you like to perform?"
 
 loop do
 user_input = gets.chomp
-  until user_input == "done"
-  input_separated = user_input.split #.join(",")
+#  until user_input == "done"
+  input_separated = user_input.split
   n = input_separated[0].to_i
   s = input_separated[1].to_s
   i = input_separated[2].to_i
-  calculator(n,s,i)
-  break
-end
+  answer = calculator(n,s,i)
+  puts answer
+  break if user_input == "done"
+#  break
+#  end
+  calc_stored << {user_input: user_input, answer: answer}
+#end
 end 
+puts "Calculations performed:"
+calc_stored.each {|n| puts "#{n[:user_input]} = #{n[:answer]}"}
+
+p calc_stored
