ff9d7d6436330761e8c980f2695a1f7ea2e387d6
“angelamarshall” <hello@angelasmarshall.com>
2016-11-30 13:48:04 -0500

    added some code for bubble sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -13,3 +13,41 @@ even = [4, 20, 46, 80]
 nothing = []
 
 p search_array(odd, 55)
+
+
+# Fibonacci Numbers
+def fib(n)
+	i = 0
+	fib_array = [0,1]
+		until fib_array.length
+			fib_array << fib_array[i] + fib_array[i + 1]
+			i += 1
+		end
+	return fib_array
+end
+
+p fib(100)
+
+
+#using the bubble sort method
+	#
+
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+bubble_sort([1,5,7,2,0,10,4])
