9d488b51a35ec41f60de84525f701bb1c66d0d8b
“angelamarshall” <hello@angelasmarshall.com>
2016-12-01 16:02:56 -0500

    added roll over method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,16 +1,20 @@
 class Puppy
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+		toy
+	end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def speak(i)
+		i.times {puts "Woof!"}
+	end
 
-  def speak(i)
-  	i.times {puts "Woof!"}
-  end
+	def roll_over
+		puts "*rolls over*"
+	end
 end
 
 
 max = Puppy.new
 max.fetch("ball")
 max.speak(2)
+max.roll_over
