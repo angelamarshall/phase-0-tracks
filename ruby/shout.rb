389c03f7d78021ed33af0ee8f82290946f09ab92
“angelamarshall” <hello@angelasmarshall.com>
2016-12-03 14:50:59 -0600

    added driver code to test methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,12 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+  	words + "!!" + ":)"
+  end
 end
 
+p Shout.yell_happily("YAYYY")
+p Shout.yell_angrily("OUCH")
