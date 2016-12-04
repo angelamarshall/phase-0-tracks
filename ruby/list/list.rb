09ad51d1a8d6a5128a69c19b2f427698424f49da
“angelamarshall” <hello@angelasmarshall.com>
2016-12-04 11:57:05 -0600

    creating todo list class

---
+++ ruby/list/list.rb
@@ -0,0 +1,20 @@
+class TodoList
+	def initialize(array)
+		@array = array 
+
+	def get_items
+		@array
+	end
+
+	def add_item(item)
+		@array.push(item)
+	end
+
+	def delete_item(item)
+		@array.delete(item)
+	end
+
+	def get_item(index)
+		@array[index]
+	end
+end
