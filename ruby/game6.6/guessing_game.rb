d8d258d6bdd4e4461f35bf72170f65dfa9076ec5
“angelamarshall” <hello@angelasmarshall.com>
2016-12-05 09:59:34 -0600

    testing with rspec

--- ruby/game6.6/guessing_game.rb
+++ ruby/game6.6/guessing_game.rb
@@ -1,10 +1,53 @@
 class GuessingGame
-	def initialize(word)
-	end
+  # attr_reader :progress :guesses
 
-	def guess(guessing_input)
-		if @guesses <1
-		else
-			@guesses -= 1
-			
+  def initialize(word)
+    @progress = "_" * word.length
+    @guesses = word.length
+    @_answer = word.downcase
+  end
+
+  def guess(guessing_input)
+    if @guesses < 1
+      if finished?
+        celebrate
+      else
+        p "You loose!"
+      end
+    else
+      @guesses -= 1
+
+      guessing_input = guessing_input[0]
+      guessing_input.downcase!
+
+      if @_answer.include?(guessing_input)
+        keep_correct_letter(guessing_input)
+        if finished?
+          celebrate
+        else
+          puts "Good job! That was a correct guess. You progress is: #{@progress}. You have #{@guesses} guesses left."
+        end
+      else
+        puts "Your guess was not correct. You have #{@guesses} left."
+      end
+    end
+  end
+
+  private
+
+  def keep_correct_letter(guessing_input)
+    guessing_input_index = @_answer.index(guessing_input)
+ #		while @_answer.include?(guessing_input)
+    @progress[guessing_input_index] = guessing_input
+  end
+
+  def finished?
+    @progress == @_answer
+  end
+
+  def celebrate
+    p "Woah! You got it right. Nice work!"
+  end
 end
+
+game = GuessingGame.new("Map")
---
+++ ruby/game6.6/guessing_game_spec.rb
@@ -0,0 +1,31 @@
+require_relative 'guessing_game'
+
+describe GuessingGame do
+  let(:game) { GuessingGame.new("map")}
+
+  it "celebrates if the user wins" do
+    "ma".each_char { |letter| game.guess(letter)}
+    expect(game.guess("p")).to eq "Woah! You got it right. Nice work!"
+  end
+
+  it "celebrates when the user guesses beyond the correct answer" do
+    "map".each_char {|letter| game.guess(letter)}
+    expect(game.guess("r")).to eq "Woah! You got it right. Nice work!"
+  end
+
+  it "encourages when user guesses correctly" do 
+    "m".each_char { |letter| game.guess(letter)}
+    expect(game.guess("a")).to eq "Good job! That was a correct guess. You progress is: #{@progress}. You have #{@guesses} guesses left."
+  end
+
+  it "lets user know when they lose the game" do
+    "ma".each_char {|letter| game.guess(letter)}
+    #guesses <= 0
+    expect(game.guess("i")).to eq "You lose!"
+  end
+
+  it "lets user know when their guess is not correct" do
+    "".each_char {|letter| game.guess(letter)}
+    expect(game.guess("o")).to eq "Your guess was not correct. You have #{@guesses} left."
+  end
+end
