b416b1af0826daf11d1f0fab2c183f62c774a1e7
“angelamarshall” <hello@angelasmarshall.com>
2016-12-11 16:52:25 -0600

    added more arrays and tested the function

--- js/algos.js
+++ js/algos.js
@@ -2,9 +2,13 @@
 // Return the length of the first element in the array since that should be the longest
 
 phrase_length = ["long phrase","longest phrase","longer phrase"]
+colors = ["red", "blue", "orange", "yellow", "purple", "pink", "green"]
+fruit = ["orange", "peach", "strawberry", "grape", "raspberry"]
 
 function findLongestWord(arr) {
   var longestWord = arr.sort(function(a, b) { return b.length - a.length; });
   return longestWord[0];
 }
 findLongestWord(phrase_length);
+findLongestWord(colors);
+findLongestWord(fruit);
