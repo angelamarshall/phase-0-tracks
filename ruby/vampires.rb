5fd903c4494fb97333715a1f4e0056290c737a47
“angelamarshall” <hello@angelasmarshall.com>
2017-01-06 12:24:33 -0600

    re-did the vampire program

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,40 +1,59 @@
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
+
+until employees<=0
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "What year were you born?"
-year = gets.chomp
+given_year = gets.chomp.to_i
 now = Time.now.year
-correct_year = now - year
+correct_year = now - given_year
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-order_bread = gets[0] == "y"
+order_bread = (gets.chomp.downcase == "yes" ? true : false)
 puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets[0] == "y"
+insurance = (gets.chomp.downcase == "yes" ? true : false)
 
-if (age == correct_year) && ((order_bread == "y") || (insurance == "y"))
-  result = "Probably not a vampire."
-else
-  result = "Results inconclusive."
+year_test = (given_year == correct_year)
+
+if year_test && (order_bread || insurance)
+  result = "Probably not a vampire"
 end
 
-if (age != correct_year) && ((order_bread != "y") || (insurance != "y"))
+if !year_test && (!order_bread || !insurance)
   result = "Probably a vampire."
-else
-  result = "Results inconclusive."
 end
 
-if (age != correct_year) && (order_bread != "y") && (insurance != "y")
+if !year_test && (!order_bread) && (!insurance)
   result = "Almost certainly a vampire."
-else
-  result = "Results inconclusive."
 end
 
 if name == "Drake Cula" || name == "Tu Fang"
   result = "Definitely a vampire."
-else
+end
+ 
+if result == nil 
   result = "Results inconclusive."
 end 
 
+puts "Please name any allergies you have one at a time. Say 'done' when you are finished:"
+      loop do
+        answer = gets.chomp 
+        if answer == "done"
+          break
+        elsif answer == "sunshine"
+          result = "Probably a vampire."
+          break
+        end
+      end
+
+puts result
+
+employees-=1
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 
 
@@ -49,7 +68,8 @@ end
 
 
 
-
+=begin
+This is my old code for this lesson:
 
 puts "How many employees will be processed?"
 employees = gets.chomp.to_i
@@ -112,5 +132,5 @@ employees = gets.chomp.to_i
   	end
 
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-
+=end
 
