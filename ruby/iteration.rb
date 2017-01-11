67075539d9f4b1a478e7acb960611d04f2dd7323
“angelamarshall” <hello@angelasmarshall.com>
2017-01-10 19:34:13 -0500

    testing different methods on arrays

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -34,7 +34,44 @@ goodbyes.each do |language, goodbye|
 end
 p goodbyes
 
+#using arrays and hashes to accomplish tasks below:
+numbers = [1,2,3,4,5,6]
 
+number_description = {
+  1 => "one",
+  2 => "two",
+  3 => "three",
+  4 => "four",
+  5 => "five",
+  6 => "six"
+}
+
+#A method that iterates through the items, deleting any that meet a certain condition 
+#(for example, deleting any numbers that are less than 5).
+p numbers
+numbers.delete_if {|number| number > 4}
+p numbers
+
+p number_description
+number_description.delete_if {|key, value| key >= 5}
+p number_description
+
+#A method that filters a data structure for only items that do satisfy a certain 
+#condition (for example, keeping any numbers that are less than 5).
+numbers.keep_if {|number| number < 4}
+p numbers
+
+
+#A different method that filters a data structure for only items satisfying a certain 
+#condition -- Ruby offers several options!
+numbers.select! { |num| num.odd? }
+p numbers 
+
+
+#A method that will remove items from a data structure until the condition in the block 
+#evaluates to false, then stops (you may not find a perfectly working option for the hash, 
+#and that's okay).
+p numbers.empty?
 
 
 
