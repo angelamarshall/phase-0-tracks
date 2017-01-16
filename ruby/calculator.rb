66df17c2db67066b8f4c612be2f3c4d592ff49dd
“angelamarshall” <hello@angelasmarshall.com>
2017-01-15 23:27:40 -0500

    prepared gps file for meeting

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,3 +1,73 @@
+def calculate(i,s,ii)
+integer1 = i.to_i
+string = s.to_s
+integer2 = ii.to_i
+  if string == '+'
+    integer1 + integer2
+  elsif string == '-'
+    integer1 - integer2
+  elsif string == '*'
+    integer1 * integer2
+  elsif s == '/'
+    integer1 / integer2
+  else
+    puts "Something went wrong. Please try again."
+  end
+end
+
+#puts calculate(4, "+", 5)
+#puts calculate(4, "-", 3)
+#puts calculate(5, "/" , 5)
+#puts calculate(5, "*", 5)
+
+stored_calculations = []
+
+puts "What calculation would you like to perform? (please put a space between each number and symbol) Reply with 'done' when you are finished."
+loop do
+calculation = gets.chomp 
+  if calculation == "done"
+    break
+  elsif 
+    calc = calculation.split(' ')
+    int1 = calc[0].to_i
+    str = calc[1].to_s
+    int3 = calc[2].to_i 
+    p calculate(int1, str, int3)
+    stored_calculations << {user_input: calculation, answer: calculate(int1, str, int3)}
+  end
+end
+
+puts "Calculations performed:"
+stored_calculations.each {|n| puts "#{n[:user_input]} = #{n[:answer]}"}
+
+# p stored_calculations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 def calculator(n, s, i)
   if s == "+"
     p n + i 
@@ -36,4 +106,4 @@ p calc_stored
 
 #not sure why the info is showing up double? 
 # tried to fix the issue of needing a space between the valies in the user input... tried split('') but then it didnt work with spaces between the values
-
+=end
