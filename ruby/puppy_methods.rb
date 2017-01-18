70884cf5c29a13ef05d466f2e36baa6d868e3a6e
“angelamarshall” <hello@angelasmarshall.com>
2017-01-17 20:31:48 -0500

    added new human class and methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,8 @@
 class Puppy
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -29,8 +33,23 @@ Fred.dog_years(3)
 Fred.high_five
 
 
+class Human
+	def initialize
+		puts "Initializing new human instance..."
+	end
 
+	def talk(i)
+		puts "blah " * i
+	end
 
+	def run
+		puts "*running*"
+	end
+end
+
+Andy = Human.new
+Andy.talk(5)
+Andy.run
 
 
 
