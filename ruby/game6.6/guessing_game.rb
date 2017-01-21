743eac745649650593c5edc0bdb3bc6385a79369
“angelamarshall” <hello@angelasmarshall.com>
2017-01-20 21:37:42 -0500

    added a method for guessing letters

--- ruby/game6.6/guessing_game.rb
+++ ruby/game6.6/guessing_game.rb
@@ -1,33 +1,105 @@
 class GuessingGame
-  attr_reader :guesses
-  attr_accessor 
+  attr_reader :guesses, :progress
+ # attr_accessor :answer
 
-  def initialize
-#    @progress = 
-#    @guesses = user_1_word.length
- #   @answer = 
+  def initialize(word)
+    @guesses = word.length
+    @answer = word.downcase
+    @progress = "_" * word.length
   end
 
-  def correct_answer(guessed_word, answer)
-    if guessed_word == answer
-      puts "Wow! You guessed the correct word. Congrats!"
-    else 
-      puts "Ugh! You did not get it right..."
+  def progress
+    @progress
+  end
+ 
+#  def separate_word
+#    @answer.split('')
+#  end
+ 
+  def guess_letters(letter)
+   until @guesses == 0 
+      if @answer.include?(letter)
+        if finished?
+          puts celebrate
+        else
+          puts "Nice work, you guessed a correct letter. Guess again."
+          letter = gets.chomp
+          keep_correct_letter(letter)
+          puts @progress
+        end
+     else
+        puts "Wrong! Guess again."
+        letter = gets.chomp
+        puts @progress
+      end 
+    @guesses -= 1
     end
+  end 
+   
+ #  def show_progress(letter)
+ #    if @answer.include?(letter)
+ #      answer_array = @answer.split('')
+ #      answer_array
+ #      @answer.tr(@progress, letter)
+ #    else
+ #      @progress
+ #    end
+ #  end
+ 
+  def keep_correct_letter(guessing_input)
+    guessing_input_index = @answer.index(guessing_input)
+    @progress[guessing_input_index] = guessing_input
+  end
+
+# @answer.tr(@progress, letter)
+ 
+=begin
+  def guessing_answer(guessed_word, answer)
+    loop do  
+      until @guesses == 0
+        if guessed_word != answer
+           puts "That's not it, try again."
+           answer = gets.chomp
+        elsif guessed_word == answer 
+           puts celebrate
+        else
+           puts taunt
+        end
+      @guesses -= 1
+      end
+    break
+    end
+  end
+=end
+    
+  def finished?
+    @answer == @progress 
+  end 
+
+  def celebrate
+    puts "Congrats! You won!!!!!"
+  end
+
+  def taunt
+    puts "Ugh! You did not get it right..."
   end
 
 end
 
 puts "Welcome to the Guessing Game!"
-game = GuessingGame.new
 
 puts "Hello user 1, what is your word?"
-user_1_word = gets.chomp
+word = gets.chomp
+game = GuessingGame.new(word)
 
-puts "Hello user 2, what is your guess?"
-user_2_word = gets.chomp
+puts "Hello user 2, what is your letter guess?"
+user_2_guess = gets.chomp
 
-game.correct_answer(user_2_word, user_1_word)
+#game.progress
+#game.guess_letters(user_2_guess)
+#game.progress
+game.keep_correct_letter(user_2_guess)
+game.guess_letters(user_2_guess)
 
 
 
